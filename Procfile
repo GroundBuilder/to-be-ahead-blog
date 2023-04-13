9290c76174911723e1e7282d7a3cc59b8f51f198
GroundBuilder <>
2023-04-13 22:50:01 +0000

    Deployment commit to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tobeaheadblog.wsgi
---
+++ everydayblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EverydayblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'everydayblog'
