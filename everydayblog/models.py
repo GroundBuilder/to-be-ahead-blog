4fdcdf68eeef7b7a6db41b59d29c649353dcb37a
GroundBuilder <erik_rynnes@hotmail.com>
2023-04-19 13:19:01 +0000

    Error 500 appears when open Post in admin django

--- everydayblog/models.py
+++ everydayblog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=220, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='everydayblog_post')
     created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
