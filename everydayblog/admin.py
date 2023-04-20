2cb804c1de672bd0332f54709d280bd929126fb4
GroundBuilder <>
2023-04-20 19:33:37 +0000

    Filter for admin in admin-site

--- everydayblog/admin.py
+++ everydayblog/admin.py
@@ -6,6 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
 
 
 @admin.register(Comment)
--- everydayblog/models.py
+++ everydayblog/models.py
@@ -4,18 +4,19 @@ from django.contrib.auth.models import User
 
 STATUS_CHOICES = (('d', 'Draft'), ('p', 'Published'), ('r', 'Public request'), ('o', 'Public'))  # 'd' and 'p' is for user in blog, 'r' is for user if they whant it to be public. 'o' for admin to decide to be plublic.
 
-class Post(models.Model):
+class Post(models.Model):    # Django db queries
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
     title = models.CharField(max_length=220, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='everydayblog_post')
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.title
 
 
-class Comment(models.Model):
+class Comment(models.Model):     # Django db queries
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     name =  models.CharField(max_length=50)
