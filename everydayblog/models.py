7a8658e3852b1fc781a57d570d028249f71e9448
GroundBuilder <>
2023-04-23 20:16:03 +0000

    Add image to blog

--- everydayblog/models.py
+++ everydayblog/models.py
@@ -5,13 +5,15 @@ from django.contrib.auth.models import User
 STATUS_CHOICES = (('d', 'Draft'), ('p', 'Published'), ('r', 'Public request'), ('o', 'Public'))  # 'd' and 'p' is for user in blog, 'r' is for user if they whant it to be public. 'o' for admin to decide to be plublic.
 
 class Post(models.Model):    # Django db queries
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
     title = models.CharField(max_length=220, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='everydayblog_post')
-    created_on = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='everydayblog_post')
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
     approved = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.title
