5d40a8d883d8e13eaec49a207982df42321f706a
GroundBuilder <>
2023-05-01 09:00:22 +0000

    Fix. Ready for deploy

--- tobeaheadblog/settings.py
+++ tobeaheadblog/settings.py
@@ -30,10 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Toggle this one below when deploy and go to Heroku and delete the DISABLE_COLLECTSTATIC in Vars.
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['to-be-ahead-blog.herokuapp.com', '8000-groundbuilder-to-be-ahea-3t49okoap2.us2.codeanyapp.com', 'localhost', 'https://groundbuilder-to-be-ahea-3t49okoap2.us2.codeanyapp.com:8000/', '8000-groundbuilder-to-be-ahea-g2qjd107m4.us2.codeanyapp.com']
 
