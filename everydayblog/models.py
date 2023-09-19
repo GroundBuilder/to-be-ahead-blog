4149f8856df5ce3811855a105411fbdc96da27ad
GroundBuilder <erik_rynnes@hotmail.com>
2023-09-19 18:34:00 +0000

    Fix. models.py is pip8 checked.

--- everydayblog/models.py
+++ everydayblog/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
-STATUS_CHOICES = (('d', 'Draft'), ('p', 'Published'), ('r', 'Public request'), ('o', 'Public'))  # 'd' and 'p' is for user in blog, 'r' is for user if they whant it to be public. 'o' for admin to decide to be plublic.
+# 'd' and 'p' is for user in blog,
+# 'r' is for user if they whant it to be public.
+# 'o' for admin to decide to be plublic.
+STATUS_CHOICES = (
+    ('d', 'Draft'),
+    ('p', 'Published'),
+    ('r', 'Public request'),
+    ('o', 'Public'))
 CATEGORY_CHOICES = ((0, 'BOL'), (1, 'EDC'), (2, 'Outher'))
 
 
@@ -11,10 +17,12 @@ class Post(models.Model):    # Django db queries
     title = models.CharField(max_length=220, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='everydayblog_post')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='everydayblog_post')
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, default='d')
     approved = models.BooleanField(default=False)
     category = models.IntegerField(choices=CATEGORY_CHOICES, default=2)
     updated_on = models.DateTimeField(auto_now=True)
@@ -24,7 +32,8 @@ class Post(models.Model):    # Django db queries
 
 
 class Comment(models.Model):     # Django db queries
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50)
@@ -35,9 +44,10 @@ class Comment(models.Model):     # Django db queries
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Delete profile when user is deleted
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)  # Delete profile when user is deleted
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'  # show how we want it to be displayed
-# Create your models here.
+        # show how we want it to be displayed
+        return f'{self.user.username} Profile'
